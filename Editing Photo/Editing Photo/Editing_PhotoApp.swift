d0cdebf3b83f6619e666fce57e5eebb39016b498
Egor <mac@macs-MacBook-Air.local>
2025-05-16 08:06:23 +0300

    создал пустой проект

---
+++ Editing Photo/Editing Photo/Editing_PhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Editing_PhotoApp.swift
+//  Editing Photo
+//
+//  Created by mac on 16.05.25.
+//
+
+import SwiftUI
+
+@main
+struct Editing_PhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
